72e7f778028c9c1bb7593b1917b278a6d5dc241c
mshibl <mohamed.shibl@hotmail.com>
2016-05-30 23:28:30 -0700

    adds access to product reading and writing

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,6 +1,6 @@
 ShopifyApp.configure do |config|
   config.api_key = ENV['API_KEY']
   config.secret = ENV['SECRET']
-  config.scope = "read_script_tags, write_script_tags"
+  config.scope = "read_script_tags, write_script_tags, read_products, write_products"
   config.embedded_app = true
 end
