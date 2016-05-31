370609adeb02a7f52c154215a0ba59948122e0a4
mshibl <mohamed.shibl@hotmail.com>
2016-05-31 01:51:06 -0700

    testing editing themes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ShopifyApp::AuthenticatedController
   def index
     @products = ShopifyAPI::Product.find(:all, :params => {:limit => 10})
     @scripts = ShopifyAPI::ScriptTag.find(:all, :params => {:limit => 10})
+    @themes = ShopifyAPI::Theme.find(:all, :params => {:limit => 10})
   end
 end
