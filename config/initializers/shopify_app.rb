8dbf939a907c1b8d3df64e092816ab5ac771b6fb
mshibl <mohamed.shibl@hotmail.com>
2016-05-30 23:32:14 -0700

    adding redirect uri

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,5 +2,6 @@ ShopifyApp.configure do |config|
   config.api_key = ENV['API_KEY']
   config.secret = ENV['SECRET']
   config.scope = "read_script_tags, write_script_tags, read_products, write_products"
+  config.redirect_uri = "https://shopify-tabs.herokuapp.com/auth/shopify/callback"
   config.embedded_app = true
 end
