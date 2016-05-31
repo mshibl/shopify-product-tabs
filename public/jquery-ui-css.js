bfee29f49767985c3936dec3fa591c5e7ac61033
mshibl <mohamed.shibl@hotmail.com>
2016-05-31 02:55:46 -0700

    adds script for adding script tag

--- public/jquery-ui-css.js
+++ public/jquery-ui-css.js
@@ -1,1 +1,2 @@
 $("head").append("<link rel=stylesheet href=https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css>")
+$("head").append("<script>$(function() { $( '#tabs' ).tabs();})</script>")
