0749ae9e54e72af755173a214b8c76bb71834034
mshibl <mohamed.shibl@hotmail.com>
2016-05-31 23:40:07 -0700

    adds tabs scss tag to head

--- public/jquery-ui-css.js
+++ public/jquery-ui-css.js
@@ -1,2 +1,4 @@
+$("head").append("{{ 'tabs.scss' | asset_url | stylesheet_tag }}")
+
 $("head").append("<link rel=stylesheet href=https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css>")
 $("head").append("<script>$(function() { $( '#tabs' ).tabs();})</script>")
