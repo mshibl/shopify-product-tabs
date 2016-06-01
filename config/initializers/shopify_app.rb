2ca64594518604e4e6a406b7ab4dbc6e3eb17bb5
mshibl <mohamed.shibl@hotmail.com>
2016-05-31 22:34:54 -0700

    removes write_users scope

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,9 +1,9 @@
 ShopifyApp.configure do |config|
   config.api_key = ENV['API_KEY']
   config.secret = ENV['SECRET']
-  config.scope = "read_script_tags, write_script_tags, read_products, write_products, read_content, write_content, read_themes, write_themes, read_customers, write_customers, read_orders, write_orders, read_fulfillments, write_fulfillments, read_shipping, write_shipping, read_analytics, read_users, write_users"
+  config.scope = "read_script_tags, write_script_tags, read_products, write_products, read_content, write_content, read_themes, write_themes, read_customers, write_customers, read_orders, write_orders, read_fulfillments, write_fulfillments, read_shipping, write_shipping, read_analytics, read_users"
   # config.redirect_uri = "https://shopify-tabs.herokuapp.com/auth/shopify/callback"
-  config.embedded_app = true
+  config.embedded_app = true 	
 
   config.scripttags = [
   	{event:'onload', src: 'https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js'},
